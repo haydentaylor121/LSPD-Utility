7d768014d775b6e3152f08e917154502d5c70bb3
haydentaylor121 <haydentaylor884@gmail.com>
2026-08-10 18:52:27 +0100

    Add training request expiry check to cron jobs

--- src/app.js
+++ src/app.js
@@ -17,6 +17,7 @@ import { initializeMusic } from './services/music/riffySetup.js';
 import { shutdownMusic } from './services/music/playerHandler.js';
 import pkg from '../package.json' with { type: 'json' };
 import { EXPECTED_SCHEMA_VERSION, EXPECTED_SCHEMA_LABEL } from './config/database/schemaVersion.js';
+import { checkTrainingRequestExpiries } from './services/trainingExpiryService.js';
 
 class TitanBot extends Client {
   constructor() {
@@ -251,6 +252,7 @@ class TitanBot extends Client {
     cron.schedule('0 6 * * *', runSafeTask('birthday_check', () => checkBirthdays(this)));
     cron.schedule('* * * * *', runSafeTask('giveaway_check', () => checkGiveaways(this)));
     cron.schedule('*/15 * * * *', runSafeTask('counter_update', () => this.updateAllCounters()));
+    cron.schedule('* * * * *', runSafeTask('training_request_expiry', () => checkTrainingRequestExpiries(this)));
   }
 
   async updateAllCounters() {
